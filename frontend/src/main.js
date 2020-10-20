ba2131f8fd190d325f40e30d10d765f00a56375a
이동현 <eagleeye0117@naver.com>
2020-10-20 13:47:07 +0900

    jwt 코드 추가 및 쿠키 설정

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import VueCookies from 'vue-cookies';
 /* eslint-disable no-new */
+
+Vue.use(VueCookies);
 new Vue({
   el: '#app',
   router, 
