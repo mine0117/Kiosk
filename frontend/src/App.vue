b22ba8ac78bad1d514197007f5ae0e81c6139a57
김성웅 <sksk9877@naver.com>
2020-10-27 00:11:55 +0000

    1026

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,51 +1,26 @@
 <template>
   <div id="app">
-    <Header :isHeader="isHeader"/>
-    <!-- <signup-from></signup-from> -->
     <router-view/>
   </div>
 </template>
 
 <script src="https://use.fontawesome.com/releases/v5.2.0/js/all.js"></script>  
 <script> 
-import './assets/css/style.scss' 
-import Header from './components/common/Header.vue'
 import constants from './lib/constants'
 
 export default {
   name: 'App',
   components: { 
-    Header,
+    // Header,
     // SignupForm,
 
   },
   created() {
-      let url = this.$route.name;
 
-      this.checkUrl(url);
   },
   watch: {
-      $route (to){
-
-          this.checkUrl(to.name);
-      }
   },
   methods : {
-      checkUrl(url) { 
-
-          let array = [
-              constants.URL_TYPE.USER.LOGIN,
-              constants.URL_TYPE.USER.JOIN,
-          ];
-
-          let isHeader = true;
-          array.map(path => {
-              if (url === path)
-                  isHeader = false;
-          })
-          this.isHeader = isHeader;
-
-      },
   },
   data: function () {
         return {
@@ -57,12 +32,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale; 
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
