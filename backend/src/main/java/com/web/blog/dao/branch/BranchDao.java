4a3e214af8dc3874cad749bdbeb503b9254e0604
박재림 <dbahck200@gmail.com>
2020-11-06 07:06:53 +0000

    Update BranchDao.java

--- backend/src/main/java/com/web/blog/dao/branch/BranchDao.java
+++ backend/src/main/java/com/web/blog/dao/branch/BranchDao.java
@@ -7,6 +7,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BranchDao extends JpaRepository<Branch, String> {
 	List<Branch> findBranchBySid(int sid);
+	void deleteByMenuid(int menuid); 
+	Branch findByMenuid(int menuid);
+
+	List<Branch> findBranchByMenuid(int menuid);
+
 	Branch findBranchBySidAndMenuid(int sid, int menuid);
 	
 
