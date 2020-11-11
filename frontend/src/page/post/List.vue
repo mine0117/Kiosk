982093903d396da0f3c2715e199cc667bd797b14
박수철 <3864617@naver.com>
2020-11-11 07:34:52 +0000

    Kiosk

--- frontend/src/page/post/List.vue
+++ frontend/src/page/post/List.vue
@@ -103,8 +103,12 @@
               align="center"
               active-nav-item-class="font-weight-bold text-uppercase text-danger"
               active-tab-class="font-weight-bold"
+              
               style="font-size: 40px"
               >
+
+
+              <div v-if = "basketRecent.length > 0">
               <b-tab 
               title-link-class="text-dark"
               title="최근먹은메뉴">
@@ -116,7 +120,7 @@
                       <div
                         v-for="(slide, index) in basketRecent"
                         :key="index"
-                        style="width: 32%; float: left"
+                        class="col-4"
                       >
                         <div>
                           <div
@@ -144,6 +148,9 @@
                   </b-tabs>
                 </div>
               </b-tab>
+              </div>
+
+
               <b-tab 
                 title-link-class="text-dark"      
                 @click="seperateCate(1, 1); rightTmp();" 
@@ -154,6 +161,7 @@
                       content-class="mt-5"
                       align="center"
                       style="font-size: 30px"
+                      active-nav-item-class="font-weight-bold text-uppercase text-danger"
                     >
                       <b-tab
                         title-link-class="text-success"
@@ -255,6 +263,7 @@
                     content-class="mt-3"
                     align="center"
                     style="font-size: 30px"
+                    active-nav-item-class="font-weight-bold text-uppercase text-danger"
                   >
                     <b-tab
                       title-link-class="text-success"
@@ -322,7 +331,6 @@
                           </div>
                       </div>
                     </div>
-
                   </b-tabs>
                 </div>
               </b-tab>
@@ -360,7 +368,7 @@ export default {
     return {
       test: true,
       menusCate: {},
-      menusCateTmp:{},
+      menusCateTmp: {},
       menuAll: {},
       y: 1,
       aa: 0,
@@ -373,7 +381,6 @@ export default {
   },
   created() {
     this.authUser();
-
     this.GetMenuInfo();
   },
   methods: {
@@ -428,19 +435,20 @@ export default {
       );
     },
     authUser() {
-      console.log("method - authUser");
       const axiosConfig = {
         headers: {
           jwtToken: `${this.$cookies.get("Auth-Token")}`,
         },
       };
-      axios
-        .post(`${constants.baseUrl}/authuser`, "", axiosConfig)
-        .then((res) => {
-          this.uid = res.data.uid;
-          this.GetMenuListRecent();
-        })
-        .catch((err) => console.log(err));
+      if (axiosConfig.headers.jwtToken != "null") {
+        axios
+          .post(`${constants.baseUrl}/authuser`, "", axiosConfig)
+          .then((res) => {
+            this.uid = res.data.uid;
+            this.GetMenuListRecent();
+          })
+          .catch((err) => console.log(err));
+      }
     },
     GetMenuInfo() {
       axios
@@ -483,18 +491,17 @@ export default {
     numberWithCommas(x) {
       return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     },
-    rightTmp(){
-      this.menusCateTmp = this.menusCate.slice(this.aa,this.aa+9);
-      console.log(this.menusCateTmp);
+    rightTmp() {
+      this.menusCateTmp = this.menusCate.slice(this.aa, this.aa + 9);
     },
-    right(){
+    right() {
       this.aa += 9;
       this.rightTmp();
     },
-    left(){
-      this.aa -=9;
+    left() {
+      this.aa -= 9;
       this.rightTmp();
-    }
+    },
   },
 };
 </script>
@@ -504,7 +511,7 @@ export default {
 * {
   font-family: "Jua", sans-serif;
   font-size: 13px;
-  line-height : 35px;
+  line-height: 35px;
 }
 .hover:hover {
   background-color: #eee;
@@ -514,7 +521,7 @@ export default {
   /* background-color: #ff0040; */
   cursor: pointer;
 }
-.cursor{
+.cursor {
   cursor: pointer;
 }
 .overflow {
@@ -522,12 +529,7 @@ export default {
   overflow-x: hidden;
   overflow-y: auto;
 }
-footer {
-  width: 100%;
-  height: 90px;
-  background: #ddd;
-  margin-top: auto;
-}
+
 .wrap {
   text-align: center;
   display: flex;
@@ -535,19 +537,24 @@ footer {
   height: 100%;
 }
 
-.tab-pills > .active > a,
-.tab-pills > .active > a:hover {
-  background-color: red;
-}
 .tmp {
   position: fixed;
   bottom: 0;
 }
-.right{
+.right {
   position: fixed;
   z-index: 160;
   bottom: 630px;
   right: 8px;
+}
+.left {
+  position: fixed;
+  z-index: 160;
+  bottom: 630px;
+  left: 5px;
+}
+p.test {
+  word-break: break-all;
 }
 .left{
   position: fixed;
