4ad887fd17c52960818d606e57630e90103f83c6
박재림 <dbahck200@gmail.com>
2020-11-11 15:33:09 +0900

    방문기록 저장 및 코드 정리

--- backend/src/main/java/com/web/blog/dao/orderlist/OrderlistDao.java
+++ backend/src/main/java/com/web/blog/dao/orderlist/OrderlistDao.java
@@ -1,7 +1,6 @@
 package com.web.blog.dao.orderlist;
 
 import java.util.*;
-import java.sql.Date;
 
 import com.web.blog.model.orderlist.Orderlist;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,16 +9,14 @@ import org.springframework.data.jpa.repository.Query;
 public interface OrderlistDao extends JpaRepository<Orderlist, String> {
 
 	List<Orderlist> findOrderlistByUidAndSid(int uid, int sid);
-	// List<Branch> findBranchBySid(int sid);
 
 	Orderlist findTop1OrderlistByUidAndSidOrderByOrderdateDesc(int uid, int sid);
 
-	// Orderlist[] findOrderlistByUidAndSidOrderByOrderdateDesc(int uid, int sid);
 	ArrayList<Orderlist> findOrderlistByUidAndSidOrderByOrderdateDesc(int uid, int sid);
+
 	ArrayList<Orderlist> findOrderlistByUidAndSidOrderByOrderdateAsc(int uid, int sid);
 
-	@Query(nativeQuery = true,value="SELECT DATE_FORMAT(o.orderdate,'%m'), SUM(b.price) FROM orderlist o join branch b on o.menuid = b.menuid GROUP BY DATE_FORMAT(o.orderdate,'%Y%m')")
+	@Query(nativeQuery = true, value = "SELECT DATE_FORMAT(o.orderdate,'%m'), SUM(b.price) FROM orderlist o join branch b on o.menuid = b.menuid GROUP BY DATE_FORMAT(o.orderdate,'%Y%m')")
 	List<?> findMonthIncome();
-	
 
-}	
+}
--- backend/src/main/java/com/web/blog/model/orderlist/Orderlist.java
+++ backend/src/main/java/com/web/blog/model/orderlist/Orderlist.java
@@ -4,17 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-
-import java.sql.Date;
 import javax.persistence.Column;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.web.blog.model.branch.Branch;
 
