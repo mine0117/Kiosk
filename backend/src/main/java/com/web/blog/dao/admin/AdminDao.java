4f03aa4a1eb91580ca5a7a8bc493a71ed4d07c1a
jy5031le@gmail.com <jy5031le@gmail.com>
2020-11-12 15:36:13 +0900

    Bug fix & Add Video

--- backend/src/main/java/com/web/blog/dao/admin/AdminDao.java
+++ backend/src/main/java/com/web/blog/dao/admin/AdminDao.java
@@ -1,8 +1,5 @@
 package com.web.blog.dao.admin;
 
-
-
-
 import java.time.LocalDateTime;
 import java.util.List;
 import com.web.blog.model.visit.Visit;
@@ -12,6 +9,9 @@ import org.springframework.data.jpa.repository.Query;
 public interface AdminDao extends JpaRepository<Visit,Integer> {
 
     Integer countByCurrenttimeBetween(LocalDateTime start, LocalDateTime end);
+    
+    @Query(nativeQuery = true, value="select count(*) from visit v WHERE DATE_FORMAT(v.currenttime ,'%Y%m%d') = DATE_FORMAT(NOW(),'%Y%m%d')")
+    Integer getCountvisitors();
     @Query(nativeQuery=true,value="SELECT DATE_FORMAT(currenttime,'%m'),count(*) FROM visit v GROUP BY DATE_FORMAT(currenttime,'%Y%m')")
     List<Visit> findVisitors();
     List<Visit> findAllByOrderByVidDesc();
